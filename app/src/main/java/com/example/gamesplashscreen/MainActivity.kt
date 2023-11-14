3fce20e708380b9a789b0e05b89884e040172100
Maftuna Vohidjonovna Domirando <maisiedev.gmail.com>
2023-11-14 17:26:47 +0500

    Initial commit

---
+++ app/src/main/java/com/example/gamesplashscreen/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.example.gamesplashscreen
+
+import androidx.compose.animation.core.Animatable
+import android.os.Bundle
+import android.view.animation.OvershootInterpolator
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Surface(color = Color(R.color.purple_200), modifier = Modifier.fillMaxSize()) {
+                Navigation()
+            }
+        }
+    }
+}
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash_screen"){
+        composable("splash_screen"){
+            SplashScreen(navController = navController)
+        }
+        composable("main_screen"){
+            Row(modifier = Modifier
+                .fillMaxSize()
+                .background(Color.Cyan)
+            ) {
+                Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally){
+                    Image(painter = painterResource(id = R.drawable.zuma_header), contentDescription = "header")
+                }
+                Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center){
+                    Text(text = "Start playing ZUMA now!", color = Color.White, fontSize = 26.sp)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(navController: NavController){
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate("main_screen")
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ){
+        Image(painter = painterResource(id = R.drawable.zuma_lego), contentDescription = "Logo", modifier = Modifier.scale(scale.value*5))
+    }
+}
